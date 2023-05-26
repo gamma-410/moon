dcf84bcf8260211657efd22c627886b38d3ea67c
gamma <gamma410.web@gmail.com>
2023-05-26 17:25:54 +0900

    Add files via upload

--- web/main.js
+++ web/main.js
@@ -1,16 +1,16 @@
-function searchG() {
+function searchGoogle() {
     var text = document.getElementById("search");
     window.location.href = "https://www.google.com/search?q=" + text.value; 
 }
-function searchB() {
+function searchBing() {
     var text = document.getElementById("search");
     window.location.href = "https://www.bing.com/search?q=" + text.value;
 }
-function searchY() {
+function searchYahoo() {
     var text = document.getElementById("search");
     window.location.href = "https://search.yahoo.co.jp/search?p=" + text.value; 
 }
-function searchD() {
+function searchDuckDuckGo() {
     var text = document.getElementById("search");
     window.location.href = "https://duckduckgo.com/?q=" + text.value;
 }
